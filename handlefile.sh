5e9b8553c3f853174b3bb42c6381894fe3731563
jinweikim <jinweikim@gmail.com>
2017-09-28 15:26:31 +0800

    second commit

--- handlefile.sh
+++ handlefile.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 dir=$1
 str=$2
-for file in ${dir}/*;
-    do
-        if [ -d $file ];
-            then $0 $file $str
-        else
-            grep $str $file
-            if [ $? -eq 0 ]
-                then echo ${file##*//}
-            fi
-        fi
-    done
+
+grep -rl  $str $dir
 
 exit 0;

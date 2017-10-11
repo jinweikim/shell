774cefaf529b65677a513a4b0e8040034c145a9a
jinweikim <jinweikim@gmail.com>
2017-10-11 13:19:32 +0800

    add repeat rate

--- check.sh
+++ check.sh
@@ -2,6 +2,9 @@
 
 file1=$1
 file2=$2
-sort $file1 $file2 | uniq -d
+main=`cat $file1 | wc -m`
+repeat=`sort $file1 $file2 | uniq -d | wc -m`
+rate=`echo "scale=2; $repeat*100/$main" | bc`
+echo "the repeat rate is $rate%"
 
 exit 0;
